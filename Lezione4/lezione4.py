c5035e3f985cf9cf986bc41267f06c821a614c0a
galbatorix060 <galbatorix060@gmail.com>
2024-04-23 13:45:40 +0200

    aggiornamento 23/04/2024

--- Lezione4/lezione4.py
+++ Lezione4/lezione4.py
@@ -141,7 +141,7 @@ messages1: list = ["eccolo", "ollalero", "viva la lazio"]
 def show_messages(l) -> str:
     for i in l:
         print(i)
-
+    return l
 show_messages(messages)
 show_messages(messages1)
 print("\n\n\n")
@@ -153,9 +153,28 @@ print("\n\n\n")
 
 print("ESERCIZIO 8-10")
 
-def send_messages()
+def send_messages(message) -> str:
+    sent_messages: list = []
+    show_messages(messages)
+    for i in message:
+        sent_messages.append(i)
+    for i in sent_messages:
+        print(f"sent message: {i}")
+    return sent_messages
+
+send_messages(messages)
+print("\n\n\n")
+
+
+#8-11. Archived Messages: Start with your work from Exercise 8-10. Call the function send_messages() with a copy of the list of messages. After calling the function, print both of your lists to show that the original list has retained its messages.
+
+
 
 print("ESERCIZIO 8-11")
+
+a = send_messages(messages1)
+print(a, "\n", messages1)
+
 
 print("ESERCIZIO 8-12")
 
