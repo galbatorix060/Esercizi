551c792258f05181cfa5a5b10a7ebbc709c88e02
galbatorix060 <galbatorix060@gmail.com>
2024-05-08 13:48:53 +0200

    08/05/2024

--- Lezione6/Classi.py
+++ Lezione6/Classi.py
@@ -1,13 +1,41 @@
 class Person:
     
-    def __init__(self, name:str,surname:str,ssn:str) -> None:
+    def __init__(self,name:str,surname:str,ssn:str,birth_date: str,birth_place: str,gender: str) -> None:
 
         """
 
         """
         self._name : str = name
         self._surname : str = surname
-        self._ssn : str = ssn
+        self._birth_date: str = birth_date
+        self._bith_place: str = birth_place
+        self._gender: str = gender
+        self.compute_ssn()
+
+    def _lower(self):
+
+        self._name.lower()
+
+    def get_ssn(self) -> str:
+
+        """
+        This function returns the ssn value
+        input: noneùreturn: self._ssn : str, the function reeturns ssn value
+
+        """
+
+        return self._ssn
+    
+    def set_ssn(self, ssn: str) -> str:
+
+        """
+        
+        This function set the ssn
+        input: ssn : str, the parameter contains the user's ssn
+        return: None
+
+        """
+        raise Exception("You cannot modify the name!")
 
     def get_name(self):
         
@@ -29,13 +57,40 @@ class Person:
         return : None
 
         """
+        self._name = name
+        self._ssn = self.compute_ssn()
+    
+    def __str__(self) -> str:
+        
+        return f"name: {self._name} surname: {self._surname} ssn: {self._ssn}"
+    
+    def compute_ssn(self) -> bool:
 
-        raise Exception("You cannot modify the name!")
+        """
+        
+        Check the ssn's correctness
+
+        """
+
+        first_three_name_char = self._name[:3]
+        last_three_surname_char = self._surname[-3:]
+
+        self._ssn = first_three_name_char + last_three_surname_char
 
 
+person1: Person = Person(name="Leonardo", surname="Marianella", ssn="MRNLRD04U20H501Z",birth_date="24/05/2023",birth_place="Rome",gender="male")
+#person2: Person = Person(name="Gigi", surname="Alfio", ssn="GIADV849IE")
+"""
+queue: list[Person] = [person1, person2]
 
-person1: Person = Person(name="Leonardo", surname="Marianella", ssn="MRNLRD04U20H501Y")
+for i in queue:
+
+    print(i.get_ssn())
+
+"""
+print(person1.get_ssn())
+#person1.set_ssn(ssn="AH500Y")
+print(str(person1))
 print(person1.get_name())
-
-person1.set_name(name = "Francesco")
 print(person1.get_name())
+print(person1.get_ssn())
