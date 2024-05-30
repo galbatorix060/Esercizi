335373ee961c474faa3cd0a9f80b55db53193edc
galbatorix060 <galbatorix060@gmail.com>
2024-05-30 10:47:03 +0200

    30/05/2024

--- Lezione12/CatalogoFilm.py
+++ Lezione12/CatalogoFilm.py
@@ -5,9 +5,45 @@ class MovieCatalog():
     def add_movie(self, director_name : str, movies : list):
         if director_name not in self.catalogo:
             self.catalogo[director_name] = movies
+        else:
+            self.catalogo[director_name] = movies
     
     def remove_movie(self, director_name, movie_name):
-        for i in self.catalogo:
-            if director_name == self.catalogo[director_name]:
-                if movie_name == self.catalogo[]
-        
+        for k, v in self.catalogo.items():
+            if director_name == k:
+                for i in v:
+                    if i == movie_name:
+                        v.remove(movie_name)
+    
+    def list_directors(self):
+        for k, v in self.catalogo.items():
+            print(k, "\n")
+
+    def get_movies_by_director(self, director_name):
+        for k, v in self.catalogo.items():
+            if director_name == k:
+                print(f"i film di {k} sono: {v}")
+    
+    def search_movies_by_title(self, title):
+        for k, v in self.catalogo.items():
+            for i in v:
+                if title == i:
+                    print(k, " ", i)
+                    break
+                else:
+                    print("nessun film con quel titolo")
+                    break
+
+catalogo : MovieCatalog = MovieCatalog()
+
+catalogo.add_movie("tarantino", ["django", "pulp fiction", "trinita"])
+catalogo.add_movie("christofer nolan", ["oppenheimer", "interstellar"])
+catalogo.add_movie("leonardo", ["Fioravanti"])
+
+catalogo.remove_movie("tarantino", "django")
+
+catalogo.list_directors()
+
+catalogo.get_movies_by_director("christofer nolan")
+
+catalogo.search_movies_by_title("oppenheimer")
