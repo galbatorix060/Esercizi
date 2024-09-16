1bc11fe7167ccfa39987f09deb2c055243e75252
Michele <michelebucci.02@gmail.com>
2024-09-16 13:48:32 +0200

    16/09/2024

--- web/ServerHttp/server.py
+++ web/ServerHttp/server.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+utenti = [["mariorossi@gmail.com", "dsrfed12e28h501r", "pippo", '0'],["elenagialli@gmail.com", "abcdef12th501m", "coca", '0'],["giannabianchi@gmail.com", "bgfmjh90e26h501i", "lalalala", '0']]
 
 api = Flask("__name__")
+
+@api.route('/registrati', methods=['GET'])
+def registrazioni():
+    utente = []
+    utente.append(request.args.get("mail"))
+    utente.append(request.args.get("codfisc"))
+    utente.append(request.args.get("password"))
+    utente.append('0')
+    if utente in utenti:
+        return render_template('regOk.html')
+    else:
+        return render_template('regKo.html')
+    
 
 @api.route('/', methods=['GET'])
 def index():
